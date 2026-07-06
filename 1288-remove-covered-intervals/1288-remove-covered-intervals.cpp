daab2f8bdc4c7e2b4ee8f162e4feab02cfc7da5e
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-07-06 17:29:29 +0530

    Time: 4 ms (28.48%), Space: 15.6 MB (18.41%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        int n=intervals.size();
-        vector<int>valid(n,1);
-        for(int i=0; i<=n-1; i++){
-            int s=intervals[i][0];
-            int e=intervals[i][1];
-            for(int j=0; j<=n-1; j++){
-                if(j!=i){
-                    int l=intervals[j][0];
-                    int r=intervals[j][1];
-                    if(s<=l && e>=r){
-                        //jth interval got covered
-                        valid[j]=0;
-                    }
-                }
+        //sort by start times
+        //same start time pe, larger end time wala pehle aayega
+        sort(intervals.begin(),intervals.end(),[](auto &a, auto &b){
+            if(a[0]==b[0]){
+                //same start time, larger end time first
+                return a[1]>b[1]; 
             }
-        }
+            return a[0]<b[0]; //agar yeh expression true hai toh a comes before b
+        });
+        int n=intervals.size();
+        int e=-1; //max end
         int cnt=0;
-        for(auto it: valid){
-            if(it==1) cnt++;
+        for(auto it: intervals){
+            if(it[1] <= e){
+                cnt++; //remove it
+            }
+            e=max(e,it[1]);
         }
-        return cnt;
+        return n-cnt;
     }
 };
