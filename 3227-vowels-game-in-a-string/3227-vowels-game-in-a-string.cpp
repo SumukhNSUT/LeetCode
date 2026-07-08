89b0a0e0cf85fc5667cbefd20cabfa092f3b6db1
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-07-08 16:19:08 +0530

    Time: 10 ms (27.97%), Space: 19.2 MB (20.65%) - LeetHub

---
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int cnt=0;
+        for(auto it: s){
+            if(it=='a' || it=='e' || it=='i' || it=='o'
+            || it=='u') cnt++;
+        }
+        if(cnt==0) return false;
+        return true;
+    }
+};
