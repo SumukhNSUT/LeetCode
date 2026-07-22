6cc559117f888d81cded4d93dadcb4b6d254299e
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-07-22 15:40:15 +0530

    Time: 66 ms (57.59%), Space: 80.9 MB (33.93%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,57 +1,28 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int ones = 0;
-        for(char c : s)
-            if(c == '1')
-                ones++;
-
-        s = "1" + s + "1";
-
-        int n = s.size();
-        int i = 0;
-
-        int ans = ones;
-
-        // Skip first 1's
-        while(i < n && s[i] == '1')
-            i++;
-
-        // Read first 0-block
-        int c10 = 0;
-        while(i < n && s[i] == '0'){
-            c10++;
-            i++;
-        }
-
-        while(i < n){
-
-            // Read middle 1-block
-            int c11 = 0;
-            while(i < n && s[i] == '1'){
-                c11++;
-                i++;
+        int n=s.size();
+        vector<int> ans;
+        int cnt=0;
+        for(int i=0; i<=n-1; i++){
+            if(s[i] == '1') cnt++;
+            if(s[i]=='0'){
+                int start=i;
+                int end=i;
+                while(i<n && s[i]=='0') {
+                    i++;
+                    end++;
+                }
+                if(s[i]=='1') cnt++;
+                ans.push_back(end-start);
             }
 
-            if(c11 == 0)
-                break;
-
-            // Read right 0-block
-            int c20 = 0;
-            while(i < n && s[i] == '0'){
-                c20++;
-                i++;
-            }
-
-            if(c20 == 0)
-                break;
-
-            ans = max(ans, ones + c10 + c20);
-
-            // Slide the window
-            c10 = c20;
         }
-
-        return ans;
+        if(ans.size() <= 1) return cnt;
+        int maxi=0;
+        for(int i=0; i<=ans.size()-2; i++){
+            maxi=max(maxi,ans[i]+ans[i+1]);
+        }
+        return maxi+cnt;
     }
 };
