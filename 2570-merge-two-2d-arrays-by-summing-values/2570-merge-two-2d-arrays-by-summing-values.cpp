1c2356823bb488eaabd8495a8927a2168b68c3cc
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-07-23 15:43:20 +0530

    Time: 0 ms (100%), Space: 14.7 MB (83.35%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        unordered_map<int,int> mp; //id, val
-        for(auto it: nums1){
-            mp[it[0]]=it[1];
-        }
-        vector<vector<int>> ans;
-        for(int i=0; i<nums2.size(); i++){
-            if(mp.count(nums2[i][0])){
-                int a=nums2[i][1] + mp[nums2[i][0]];
-                ans.push_back({nums2[i][0],a});
-                mp.erase(nums2[i][0]);
+        vector<vector<int>>ans;
+        int i=0;
+        int j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i][0] == nums2[j][0]){
+                ans.push_back({nums1[i][0], nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }else if(nums1[i][0] < nums2[j][0]){
+                ans.push_back({nums1[i][0],nums1[i][1]});
+                i++;
             }else{
-                ans.push_back({nums2[i][0],nums2[i][1]});
+                ans.push_back({nums2[j][0],nums2[j][1]});
+                j++;
             }
         }
 
-        for(auto it: mp){
-            ans.push_back({it.first,it.second});
+        while(i<nums1.size()){
+            ans.push_back({nums1[i][0],nums1[i][1]});
+            i++;
         }
-        sort(ans.begin(), ans.end());
+        while(j<nums2.size()){
+            ans.push_back({nums2[j][0],nums2[j][1]});
+            j++;
+        }
+
         return ans;
     }
 };
