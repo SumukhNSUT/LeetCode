5e81ae17eed97a723aec157856c9e8648655a342
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-07-25 14:59:40 +0530

    Time: 40 ms (32.41%), Space: 36.2 MB (68.05%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,26 +1,47 @@
 class Solution {
-    int fn(int idx, int target, vector<int>&coins, vector<vector<int>>&dp){
-        //base case
-        if(idx==0){
-            if(target%coins[0] == 0) return 1;
-            else return 0;
-        }
-        //general case
-        if(dp[idx][target] != -1) return dp[idx][target];
-        int pick=0;
-        if(coins[idx] <= target){
-            pick=fn(idx, target-coins[idx], coins, dp);
-        }
-        int npick=fn(idx-1,target,coins,dp);
-        dp[idx][target]=pick+npick;
-        return dp[idx][target];
-    }
+    // //memo
+    // int fn(int idx, int target, vector<int>&coins, vector<vector<int>>&dp){
+    //     //base case
+    //     if(idx==0){
+    //         if(target%coins[0] == 0) return 1;
+    //         else return 0;
+    //     }
+    //     //general case
+    //     if(dp[idx][target] != -1) return dp[idx][target];
+    //     int pick=0;
+    //     if(coins[idx] <= target){
+    //         pick=fn(idx, target-coins[idx], coins, dp);
+    //     }
+    //     int npick=fn(idx-1,target,coins,dp);
+    //     dp[idx][target]=pick+npick;
+    //     return dp[idx][target];
+    // }
 public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         //fn(idx)=> idx se 0 tak
         //dp[idx][target]
         vector<vector<int>>dp(n, vector<int>(amount+1,-1));
-        return fn(n-1,amount,coins,dp);
+        //tabulation
+        //base case
+        for(int target=0; target<=amount; target++){
+            if(target%coins[0] == 0){
+                dp[0][target]=1;
+            }else{
+                dp[0][target]=0;
+            }
+        }
+        //general case
+        for(int idx=1; idx<=n-1; idx++){
+            for(int target=0; target<=amount; target++){
+                unsigned int pick=0;
+                if(coins[idx] <= target){
+                    pick=dp[idx][target-coins[idx]];
+                }
+                unsigned int npick=dp[idx-1][target];
+                dp[idx][target]=pick+npick;
+            }
+        }
+        return dp[n-1][amount];
     }
 };
