0ec892d161427ec06706f400be56774715eb82fa
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-07-31 21:31:21 +0530

    Time: 5 ms (79.89%), Space: 28.4 MB (31.38%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int n=word.size();
-        unordered_map<char,int> mp;
-        for(auto it: word) mp[it]++;
-
-        vector<pair<int,char>>v;
-        for(auto it: mp){
-            v.push_back({it.second,it.first});
+        vector<int> v(26,0);
+        for(int i=0; i<n; i++){
+            v[word[i]-'a']++;
         }
         sort(v.rbegin(), v.rend());
 
@@ -20,17 +17,17 @@ public:
         for(int i=0; i<v.size(); i++){
             //top 8 most frequent
             if(i>=0 && i<=7){
-                ans+=v[i].first;
+                ans+=v[i];
             }
             else if(i>=8 && i<=15){
-                ans+=2*v[i].first;
+                ans+=2*v[i];
             }
             else if(i>=16 && i<=23){
-                ans+=3*v[i].first;
+                ans+=3*v[i];
             }
             else{
                 //for i=24,25
-                ans+=4*v[i].first;
+                ans+=4*v[i];
             }
         }
         return ans;
