2d6f6ab7fe157285746f892c683b2d8f57c9101f
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-08-01 13:18:00 +0530

    Time: 0 ms (100%), Space: 10.2 MB (45.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,24 +1,25 @@
 class Solution {
-    bool fn(int s, int e, int p1, int p2, vector<int>& nums, bool flag) {
+    int fn(int s, int e, vector<int>& nums, vector<vector<int>>&dp) {
         //base case
-        if (s > e) return p1>=p2;
+        if (s==e) return nums[s];
 
-        if (flag) {
-            //player 1 turn
-            bool left = fn(s + 1, e, p1 + nums[s], p2, nums, false);
-            bool right = fn(s, e - 1, p1 + nums[e], p2, nums, false);
-            return left || right;
-        } else {
-            //player 2 turn
-            bool left = fn(s + 1, e, p1, p2 + nums[s], nums, true);
-            bool right = fn(s, e - 1, p1, p2 + nums[e], nums, true);
-            return left && right;
-        }
+        if(dp[s][e]!=-1) return dp[s][e];
+        //pick left
+        int left=nums[s]-fn(s+1,e,nums,dp);
+        //pick right
+        int right = nums[e]-fn(s,e-1,nums,dp);
+        dp[s][e]=max(left, right);
+        return dp[s][e];
     }
 
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        return fn(0, n- 1, 0, 0, nums, true);
+        //[s,e] tak max score diff >= 0
+        //p1 try to maximize score diff
+
+        //dp[s][e]
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return fn(0,n-1,nums,dp) >= 0;
     }
 };
