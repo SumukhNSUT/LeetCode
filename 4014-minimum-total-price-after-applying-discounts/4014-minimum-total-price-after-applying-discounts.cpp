44502f840894a2600d3ce43bc5cb96800b8eea0a
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-08-09 12:39:57 +0530

    Time: 184 ms (24.99%), Space: 225.8 MB (35.27%) - LeetHub

---
+++ 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double minPrice(vector<int>& prices, vector<int>& discounts) {
+        double ans=0;
+        int i=0;
+        int j=0;
+        sort(prices.rbegin(), prices.rend());
+        sort(discounts.rbegin(), discounts.rend());
+        while(i<prices.size() && j<discounts.size()){
+            int p=prices[i];
+            int d=discounts[j];
+            double fp= (double) ( (double) p* (double)(100-d))/ (double) 100;
+            ans+=fp;
+            i++;
+            j++;
+        }
+        
+        if(i==prices.size()) return ans;
+        else{
+            while(i<prices.size()){
+                ans+=double(prices[i]);
+                i++;
+            }
+        }
+        return ans;
+    }
+};
