3cd0a35bf7f8ca40b251977e3b552d5b9f96202b
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-08-10 13:51:07 +0530

    Time: 0 ms (100%), Space: 12 MB (6.64%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,28 +1,32 @@
 class Solution {
-    // dp[i][j] => i,j se 0,0 pe aa rha
-    // up and left are options
-    // top down approach
-    private:
-    int fn(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&dp, 
-    int n, int m){
-        if(i>=0 && j>=0 && grid[i][j]==1) return 0; //obstacle, invalid path
+private:
+    int fn(int row, int col, vector<vector<int>>&mat, vector<vector<int>> &dp){
+        int m=mat.size();
+        int n=mat[0].size();
+        //base case
+        if(row==m-1 && col==n-1){
+            return 1;
+        }
 
-        //base case(reached target)
-        if(i==0 && j==0) return 1; // 0,0 se 0,0 tak paths
-
-        if(i<0 || j<0) return 0; //invalid path
-        if(dp[i][j] != -1) return dp[i][j];
-        int up=fn(i-1,j,grid,dp,n,m);
-        int left=fn(i,j-1,grid,dp,n,m);
-        dp[i][j]=up+left;
-        return dp[i][j];
+        int d=0;
+        int r=0;
+        if(dp[row][col] != -1) return dp[row][col];
+        //down
+        if(row <= m-2 && mat[row+1][col] != 1){
+            d=fn(row+1,col,mat,dp);
+        }
+        //right
+        if(col <= n-2 && mat[row][col+1] != 1){
+            r=fn(row,col+1,mat,dp);
+        }
+        return dp[row][col]=d+r;
     }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n=obstacleGrid.size();
-        int m=obstacleGrid[0].size();
-        vector<vector<int>>dp(n, vector<int>(m,-1));
-        // 1 obstacle and 0 pe can move
-        return fn(n-1,m-1,obstacleGrid,dp,n,m);
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1]==1) return 0;
+        vector<vector<int>>dp(m,vector<int>(n, -1));
+        return fn(0,0,obstacleGrid,dp);
     }
 };
