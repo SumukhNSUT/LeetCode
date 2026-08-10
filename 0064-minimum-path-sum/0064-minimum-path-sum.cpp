41ad30cc0d052400c227ebea634b9de13da9ff1d
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-08-10 13:57:12 +0530

    Time: 3 ms (30.38%), Space: 16.3 MB (62.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
-    int fn(int i, int j, int n, int m, vector<vector<int>>&grid,
-    vector<vector<int>>&dp){
+private:
+    int fn(int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp){
         //base case
-        if(i==0 && j==0) return grid[0][0];
-        if(i<0 || j<0) return INT_MAX;
-
-        //for general i, j
-        if(dp[i][j] != -1) return dp[i][j];
-        int up=INT_MAX;
-        int left=INT_MAX;
-        if(i>0)up=grid[i][j]+fn(i-1,j,n,m,grid,dp);
-        if(j>0)left=grid[i][j]+fn(i,j-1,n,m,grid,dp);
-        dp[i][j]=min(up,left);
-        return dp[i][j];
+        int m=grid.size();
+        int n=grid[0].size();
+        if(row==m-1 && col==n-1){
+            return grid[row][col];
+        }
+        if(dp[row][col] != -1) return dp[row][col];
+        int d=1e9;
+        int r=1e9;
+        if(row <= m-2){
+            d=grid[row][col]+fn(row+1,col,grid,dp);
+        }
+        if(col <= n-2){
+            r=grid[row][col]+fn(row,col+1,grid,dp);
+        }
+        return dp[row][col]=min(d,r);
     }
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int i=n-1;
-        int j=m-1;
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        //fn(i,j)=> i,j se 0,0 tak min path sum
-        return fn(i,j,n,m,grid,dp);
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return fn(0,0,grid,dp);
     }
 };
