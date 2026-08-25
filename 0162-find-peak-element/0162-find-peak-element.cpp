7b5d74c4806916ed3ed3279681ea767d42fa7e92
Sumukh Bansal <142471848+SumukhNSUT@users.noreply.github.com>
2026-08-26 02:40:25 +0530

    Time: 0 ms (100%), Space: 12.6 MB (46.13%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -8,11 +8,27 @@ public:
         if(nums[0] > nums[1]) return 0;
         if(nums[n-1] > nums[n-2]) return n-1;
 
-        for(int i=1; i<=n-2; i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                return i;
+        int s=1;
+        int e=n-2;
+        while(s <= e){
+            int mid=(s+e)/2;
+            //check if mid is the peak
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            //upward slope
+            else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1]){
+                s=mid+1; //aage hai peak
+            }
+            //downward slope
+            else if(nums[mid]<nums[mid-1] && nums[mid]>nums[mid+1]){
+                e=mid-1; //peeche hai peak
+            }else{
+                s=mid+1; //valley case
+                //e=mid-1 also works, move either side
             }
         }
+
         return -1;
     }
 };
